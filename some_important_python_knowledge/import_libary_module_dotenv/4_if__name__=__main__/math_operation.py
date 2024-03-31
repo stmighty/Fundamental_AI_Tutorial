2f5f7836e0564bff46ab61a42d75349e3c6913d8
Sirasit Tanrattanawong <might.la.fr@gmail.com>
2024-04-01 02:36:35 +0700

    restruct

---
+++ some_important_python_knowledge/import_libary_module_dotenv/4_if__name__=__main__/math_operation.py
@@ -0,0 +1,16 @@
+# math_operations.py
+
+def add(a, b):
+    """Add two numbers."""
+    return a + b
+
+def multiply(a, b):
+    """Multiply two numbers."""
+    return a * b
+
+if __name__ == "__main__":
+    # This code block will only execute when the script is run directly,
+    # not when it is imported as a module in another script.
+    print("Demonstrating module functionality:")
+    print("Add 2 + 3 =", add(2, 3))
+    print("Multiply 2 * 3 =", multiply(2, 3))
---
+++ some_important_python_knowledge/import_libary_module_dotenv/4_if__name__=__main__/use_operation.py
@@ -0,0 +1,11 @@
+# use_operations.py
+
+import math_operation
+
+print("Using the add function from math_operations.py to add 5 and 7:")
+print(math_operation.add(5, 7))
+
+
+
+
+# if __name__ == __main__ is not excecuted.
