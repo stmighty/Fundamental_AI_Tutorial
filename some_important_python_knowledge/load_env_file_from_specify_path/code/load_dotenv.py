b821a683b07e7c64262d46842cd349c72c938a02
Sirasit Tanrattanawong <might.la.fr@gmail.com>
2024-03-31 23:54:23 +0700

    update

--- some_important_python_knowledge/load_env_file_from_specify_path/code/load_dotenv.py
+++ some_important_python_knowledge/load_env_file_from_specify_path/code/load_dotenv.py
@@ -6,6 +6,10 @@ from pathlib import Path
 dotenv_path = Path('../.env')
 config = load_dotenv(dotenv_path=dotenv_path)
 
+print("type of os.environ is", type(os.environ))
+print(os.environ)
+print()
+
 
 COHERE_API_KEY = os.environ.get("COHERE_API_KEY")
 print(COHERE_API_KEY)
