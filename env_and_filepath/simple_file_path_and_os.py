96010bc7ffe007407f068deb33730f9f05a8abff
Sirasit Tanrattanawong <might.la.fr@gmail.com>
2024-04-08 09:30:45 +0700

    update

--- env_and_filepath/simple_file_path_and_os.py
+++
@@ -1,5 +0,0 @@
-# you should run via terminal using python <filename.py> the file path depends on where you run the command in terminal
-import json
-from pathlib import Path
-directory_path = Path.cwd()
-print("current path is :", directory_path)
