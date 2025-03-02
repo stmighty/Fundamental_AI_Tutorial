922d6416aebb6a8ad95641494f423564c2d5b0b2
Sirasit Tanrattanawong <might.la.fr@gmail.com>
2025-03-03 00:14:02 +0700

    update

---
+++ some_important_python_knowledge/etc/and_operator_short_circuit_evaluation.py
@@ -0,0 +1,5 @@
+a = "A"
+b = "B"
+result = a and b
+
+print(result)               # logical operators like && (and in Python, and) evaluate the left-hand operand first. If it's falsy, they immediately return that value without evaluating the right-hand operand. If it's truthy, they return the right-hand operand.
